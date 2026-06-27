d0446497b839f142c3664cda79f1c0c3531e75cf
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2026-06-27 13:21:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from pygame import *
 from random import randint
-
-a = 5
-b = a + 3
+import numpy as np
+import sounddevice as sd
 
 init()
 size = 1200, 800
 window = display.set_mode(size)
 clock = time.Clock()
+
+sr = 16000
+block = 256
+mic_level = 0.0
+
+
+
+def audio_cd(indata, frame, time, status):
+    global mic_level
+    if status:
+        return
+    rms = float(np.sqrt(np.mean(indata**2)))
+    mic_level = 0.85 * mic_level + 0.15 * rms
 
 background = image.load("images/background.png")
 background = transform.scale(background, size)
@@ -20,7 +32,7 @@ truba = image.load("images/truba.png")
 score = 0
 lose = False
 main_font = font.Font(None, 36)
-y_vel = 2
+# y_vel = 2
 
 player_rect = Rect(150, size[1] // 2 - 100, 100, 100)
 
@@ -39,49 +51,68 @@ def generate_pipes(count, pipe_width=120, gap=200, min_height=50, max_height=440
 
 pipes = generate_pipes(150)
 
-while True:
-    for e in event.get():
-        if e.type == QUIT:
-            quit()
+y_vel = 0.0
+gravity = 0.6
+THRESH = 0.01
+IMPULSE = -8.0
 
-    window.blit(background, (0, 0))
 
-    window.blit(player, (player_rect.x, player_rect.y))
 
-    if len(pipes) < 8:
-        pipes = generate_pipes(150)
+with sd.InputStream(samplerate=sr, blocksize=block, channels=1, callback=audio_cd):
+    while True:
+        for e in event.get():
+            if e.type == QUIT:
+                quit()
 
-    for pipe in pipes:
-        pipe.x -= 10
+        if mic_level > THRESH:
+            y_vel = IMPULSE
+        y_vel += gravity
+        player_rect.y += int(y_vel)
 
-        if pipe.y == 0:
-            pipe_img = transform.scale(truba, (pipe.width, pipe.height))
-            window.blit(pipe_img, (pipe.x, pipe.y))
+        window.blit(background, (0, 0))
+
+        window.blit(player, (player_rect.x, player_rect.y))
+
+        if len(pipes) < 8:
+            pipes = generate_pipes(150)
+
+        for pipe in pipes:
+            pipe.x -= 10
+
+            if pipe.y == 0:
+                pipe_img = transform.scale(truba, (pipe.width, pipe.height))
+                window.blit(pipe_img, (pipe.x, pipe.y))
+            else:
+                pipe_img = transform.scale(truba, (pipe.width, pipe.height))
+                pipe_img = transform.flip(pipe_img, False, True)
+                window.blit(pipe_img, (pipe.x, pipe.y))
+
+            if pipe.x <= -100:
+                pipes.remove(pipe)
+                score += 1
+
+            if player_rect.colliderect(pipe):
+                lose = True
+
+        score_text = main_font.render(f"{int(score)}", 1, "white")
+        center_text = size[0] // 2 - score_text.get_rect().w // 2
+        window.blit(score_text, (center_text, 40))
+
+        clock.tick(60)
+        display.update()
+
+        keys = key.get_pressed()
+        if keys[K_w] and not lose:
+            player_rect.y -= 15
+        if keys[K_s] and not lose:
+            player_rect.y += 15
+
+        if lose and wait > 1:
+            # player_rect.y += y_vel
+            # y_vel *= 1.1
+            for pipe in pipes:
+                pipe.x +=8
+            wait -=1
         else:
-            pipe_img = transform.scale(truba, (pipe.width, pipe.height))
-            pipe_img = transform.flip(pipe_img, False, True)  
-            window.blit(pipe_img, (pipe.x, pipe.y))
-
-        if pipe.x <= -100:
-            pipes.remove(pipe)
-            score += 1
-
-        if player_rect.colliderect(pipe):
-            lose = True
-
-    score_text = main_font.render(f"{int(score)}", 1, "white")
-    center_text = size[0] // 2 - score_text.get_rect().w // 2
-    window.blit(score_text, (center_text, 40))
-
-    clock.tick(60)
-    display.update()
-
-    keys = key.get_pressed()
-    if keys[K_w] and not lose:
-        player_rect.y -= 15
-    if keys[K_s] and not lose:
-        player_rect.y += 15
-
-    if lose:
-        player_rect.y += y_vel
-        y_vel *= 1.1
+            lose = False
+            wait = 40
